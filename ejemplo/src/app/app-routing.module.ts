f6ec42d4f442e3f32e01875b739438c30b4f9f0b
SuarezRuben <suarezruben20@gmail.com>
2018-01-03 04:15:44 -0300

    se subio la estructura

--- ejemplo/src/app/app-routing.module.ts
+++ ejemplo/src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LogRegComponent } from './log-reg/log-reg.component';
 import { RegistroComponent } from './registro/registro.component';
 import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { CanchasComponent } from './canchas/canchas.component';
+import { EquiposComponent } from './equipos/equipos.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes : Routes =[
   { path:'registro' , component:RegistroComponent },
-  { path:'iniciar_sesion', component:IniciarSesionComponent }
+  { path:'iniciar_sesion', component:IniciarSesionComponent },
+  { path:'inicio', component:InicioComponent },
+  { path:'canchas', component:CanchasComponent },
+  { path:'equipos', component:EquiposComponent }
 ];
 
 @NgModule({
