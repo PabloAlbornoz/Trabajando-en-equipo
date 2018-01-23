58605b3f2dd5a8ebd897940bfecbc8de16debd14
RubenSuarez <suarezruben20@gmail.com>
2018-01-23 19:26:12 -0300

    Prueba

---
+++ PAGINA_ELITEFM/prueba/index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<title>...: LRT 809 Elite FM 101.5 Mhz - Barrio Argentino - General Cabrera :...</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8 />
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<meta http-equiv="Content-Type" content="text/html; UTF-8">
+<meta charset="UTF-8">
+<META NAME="DC.Language" SCHEME="RFC1766" CONTENT="Spanish">
+<META NAME="AUTHOR" CONTENT="Elite FM">
+<META NAME="REPLY-TO" CONTENT="fmelite@hotmail.com">
+<LINK REV="made" href="mailto:fmelite@hotmail.com">
+<META NAME="DESCRIPTION" CONTENT="Promoción de Artistas Independientes de todo el Mundo. Tango, Jazz, Blues, Pop, Rock, Disco y Retro.">
+<META NAME="KEYWORDS" CONTENT="elite,LRT809,radio fm 101.5,barrio argentino,general cabrera,córdoba,transmisión en vivo,artistas independientes,musicos independientes,música,información,medio ambiente,escuchar radio Internet,escuchar música,radio online,producción radial,noticias de elite,musica clasica,tango,jazz,blues,pop,rock,disco,retro,k-pop,j-rock,radios en general cabrera,mundo,organizaciones,auspiciantes,produccion de medios radiales,audiencia,app exclusiva,facebook,twitter,linkedin,google+,tunein,raddios.com,programacion,demos,amigos,blog,anuncios google,contacto">
+<META NAME="Resource-type" CONTENT="Homepage">
+<META NAME="DateCreated" CONTENT="Wed, 23 April 2008 00:00:00 GMT-3">
+<META NAME="Revisit-after" CONTENT="1 days">
+<META NAME="robots" content="ALL">
+<meta name="verify-v1" content="I9Q1nBl5J+oMgWRGas2kL/AwBbkJxzobt5pDlAUjAxc=" >
+<meta name="verify-v1" content="agXffEPtLZKEkmOAiMq0XLjO1WnjHAXUJGtlwjk+1Dk=" />
+<!-- estilo copado-->
+<link rel="stylesheet" type="text/css" href="estilo.css" />
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W6JW976');</script>
+<!-- End Google Tag Manager -->
+<script> 
+function abrir(url) { 
+open(url,'','top=80,left=350,width=420,height=650') ; 
+} 
+</script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-31734022-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+<link rel="shortcut icon" href="http://www.elitefm.com.ar/favicon.ico">
+<script src="http://static.dudamobile.com/DM_redirect.js" type="text/javascript"></script>
+<script type="text/javascript">DM_redirect("http://mobile.dudasite.com/site/elitefm1015");</script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!--<style type="text/css" media="screen">
+		html, body { height:100%; background-color: blue;}
+		body { margin:0; padding:0; overflow-x:hidden;overflow-y:auto; }
+		#flashContent { width:100%; height:100%; }
+		</style>-->
+<!-- Start WOWSlider.com HEAD section -->
+<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+<script type="text/javascript" src="engine1/jquery.js"></script>
+<!-- End WOWSlider.com HEAD section -->
+<script async src="file://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-7907820128722621",
+    enable_page_level_ads: true
+  });
+</script>
+</head>
+<body>
+  
+<!--<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W6JW976"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>-->
+  <div class="menu">
+  <ul>
+    <li><a href="http://www.elitefm.com.ar/programacion.php"> Programacion </a></li>   
+    
+    <li><a href="http://www.elitefm.com.ar/amigos.php">  Amigos  </a></li>
+    <li><a href="http://www.elitefm.com.ar/bibliot_audios.php">  Biblioteca  de   audios </a></li>
+    <li><a href="http://www.elitefm.com.ar/noticias.php">  Noticias   </a></li>
+    
+    <li><a href="javascript:abrir('nosotros.html')"> Nosotros </a>
+      <ul>
+        <li><a href="http://www.elitefm.com.ar/demos.php">  Demos  </a></li>
+        <li><a href="http://elitegeneralcabrera.blogspot.com.ar/"> Blog    </a></li>
+        <li><a href="javascript:abrir('formulario.html')">  Contacto </a></li>
+      </ul>
+
+
+    </li>
+
+
+    
+  </ul>
+</div>
+  <div class="image_inicio">
+    <img src="img/heater2.jpg">
+  </div>
+  
+  <div class="wrapper">
+    <p>Rumbo a los 10 Años de Transmision Ininterrumpida Online de Artistas Independientes de Tango, Jazz, Blues, Rock & Pop. <span style="color:red"> Gracias a todas las organizaciones y auspiciantes que apoyan este proyecto</span><span style="color: blue"> Gracias a toda la audiencia que se va sumando diariamente. </span><span style="color: orange">Y a los músicos independientes que envían sus obras desde todo el mundo</span></p>
+  </div>
+  <div id="wowslider-container1">
+<div class="ws_images"><ul>
+    <li><a href="https://www.facebook.com/groups/277030265716387/" target="_blank"><img src="data1/images/portada_piloto_mobile.jpg" alt="El Piloto Automático Martes y Domingo 22HS" title="El Piloto Automático Martes y Domingo 22HS" id="wows1_0"/></a></li>
+    <li><a href="https://www.facebook.com/groups/329603888586/" target="_blank"><img src="data1/images/grupo_amigos_mobile.jpg" alt="Amigos de Elite FM" title="Amigos de Elite FM" id="wows1_1"/></a></li>
+    <li><a href="https://www.facebook.com/groups/475259752537055/" target="_blank"><img src="data1/images/grupo_noticias_mobile.jpg" alt="Noticias de Elite Lunes a Viernes 10HS" title="Noticias de Elite Lunes a Viernes 10HS" id="wows1_2"/></a></li>
+    <li><a href="https://www.facebook.com/groups/182811471736149/" target="_blank"><img src="data1/images/grupo_hitsstory_mobile.jpg" alt="HitsStory Sábados 20HS" title="HitsStory Sábados 20HS" id="wows1_3"/></a></li>
+    <li><a href="https://www.facebook.com/groups/329603888586/" target="_blank"><img src="data1/images/banner_2l0c0s_arg_mobile.jpg" alt="2 L0C0S D3 M1$RD4 Viernes 22HS" title="2 L0C0S D3 M1$RD4 Viernes 22HS" id="wows1_4"/></a></li>
+    <li><a href="https://www.facebook.com/groups/231350906909232/" target="_blank"><img src="data1/images/grupo_blues_mobile.jpg" alt="Sentimiento Blues & Ese Negro Blues Domingos 11HS" title="Sentimiento Blues & Ese Negro Blues Domingos 11HS" id="wows1_5"/></a></li>
+    <li><img src="data1/images/grupo_alien_mobile.jpg" alt="El Rincón del Alienígena" title="El Rincón del Alienígena" id="wows1_6"/></li>
+  </ul></div>
+  <div class="ws_bullets"><div>
+    <a href="#" title="El Piloto Automático Martes y Domingo 22HS"><span><img src="data1/tooltips/portada_piloto_mobile.jpg" alt="El Piloto Automático Martes y Domingo 22HS"/>1</span></a>
+    <a href="#" title="Amigos de Elite FM"><span><img src="data1/tooltips/grupo_amigos_mobile.jpg" alt="Amigos de Elite FM"/>2</span></a>
+    <a href="#" title="Noticias de Elite Lunes a Viernes 10HS"><span><img src="data1/tooltips/grupo_noticias_mobile.jpg" alt="Noticias de Elite Lunes a Viernes 10HS"/>3</span></a>
+    <a href="#" title="HitsStory Sábados 20HS"><span><img src="data1/tooltips/grupo_hitsstory_mobile.jpg" alt="HitsStory Sábados 20HS"/>4</span></a>
+    <a href="#" title="2 L0C0S D3 M1$RD4 Viernes 22HS"><span><img src="data1/tooltips/banner_2l0c0s_arg_mobile.jpg" alt="2 L0C0S D3 M1$RD4 Viernes 22HS"/>5</span></a>
+    <a href="#" title="Sentimiento Blues & Ese Negro Blues Domingos 11HS"><span><img src="data1/tooltips/grupo_blues_mobile.jpg" alt="Sentimiento Blues & Ese Negro Blues Domingos 11HS"/>6</span></a>
+    <a href="#" title="El Rincón del Alienígena"><span><img src="data1/tooltips/grupo_alien_mobile.jpg" alt="El Rincón del Alienígena"/>7</span></a>
+  </div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.com/vi">css slider</a> by WOWSlider.com v8.7</div>
+<div class="ws_shadow"></div>
+<script type="text/javascript" src="engine1/wowslider.js"></script>
+<script type="text/javascript" src="engine1/script.js"></script>
+</div>  
+</body>
+</html>
+<script language="JavaScript" src="stars.js"></script>
